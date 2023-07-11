337d924bb9a9d05a5d800c52eaed5d0aeee83c0b
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-07-11 11:13:51 +0530

    movies

--- scriptmovie.js
+++ scriptmovie.js
@@ -1,28 +1,32 @@
 const sec = document.querySelectorAll(".s");
-let i = 1;
+let i = 0;
 const right = document.getElementById("right");
 const left = document.getElementById("left");
 right.addEventListener("click", () => {
+    
+    ++i;
     console.log(i);
-  
+    if (i == sec.length) {
+      i = 0;
+    }
   sec.forEach((e) => {
     e.style.display = "none";
   });
   sec[i].style.display = "block";
-  ++i;
-  if (i == sec.length) {
-    i = 0;
-  }
+  
+  
 });
 left.addEventListener("click", () => {
+   --i;
     console.log(i);
+    if (i == -1) {
+      i = sec.length-1;
+    }
     
     sec.forEach((e) => {
       e.style.display = "none";
     });
     sec[i].style.display = "block";
-    --i;
-    if (i == -1) {
-        i = sec.length-1;
-      }
+    
+    
   });
