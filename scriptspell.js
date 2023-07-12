f84e1e29ab7c0f11dc5a745763af61017dd00c26
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-07-12 09:42:25 +0530

    add

--- scriptspell.js
+++ scriptspell.js
@@ -1,19 +1,33 @@
 const spells = document.querySelectorAll(".s");
 let i = 1;
 const btn = document.querySelector(".next");
+const back = document.querySelector(".back");
 btn.addEventListener("click", () => {
   /*for (i = 0; i < spells.length; i++) {
     console.log(spells[i].innerHTML);
   }*/
-  if(i==spells.length)
-  {
-    i=0;
-  }
-  spells.forEach((e)=>
-  {
-    e.style.display="none";
-
-  })
-  spells[i].style.display="block";
   ++i;
+  console.log(i);
+
+  if (i == spells.length) {
+    i = 0;
+  }
+  spells.forEach((e) => {
+    e.style.display = "none";
+  });
+  spells[i].style.display = "block";
+  
+});
+back.addEventListener("click", () => {
+  --i;
+  console.log(i);
+
+  if (i == -1) {
+    i = spells.length-1;
+  }
+  spells.forEach((e) => {
+    e.style.display = "none";
+  });
+  spells[i].style.display = "block";
+  
 });
